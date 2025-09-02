33a356d6e3baa601d467ade98c906d9467d8646e
Loch-arch <it24100314@my.sliit.lk>
2025-09-02 23:34:33 +0530

    IT24100314_Lab_06

---
+++ Lab Sheet 06/IT24100314.R
@@ -0,0 +1,19 @@
+setwd("C:\\Users\\loch2\\OneDrive\\Desktop\\IT24100314")
+
+#Q1
+#i.
+#binomial distribution
+
+#ii.
+pbinom(46,50,0.85,lower.tail =FALSE)
+
+
+#Q2
+#i.
+#number of customer calls per hour
+
+#ii.
+#poisson distribution
+
+#iii.
+dpois(15,12)
